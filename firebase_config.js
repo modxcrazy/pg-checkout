770ae02eaa9ae71efd599d8910925dea4ecd22ef
modxcrazy <in99soft.dev.pvt.ltd@gmail.com>
2025-04-11 05:27:58 +0530

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -1,16 +1,19 @@
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-import { getDatabase, ref, get, child, onValue } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
+import { getDatabase, ref, set, get, child, onValue } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCn553qWsVz2VF1dZ4Ji5OkQDGFvMORbJE",
   authDomain: "pg-data-ed1c2.firebaseapp.com",
+  databaseURL: "https://pg-data-ed1c2-default-rtdb.firebaseio.com",
   projectId: "pg-data-ed1c2",
   storageBucket: "pg-data-ed1c2.appspot.com",
   messagingSenderId: "884858492190",
-  appId: "1:884858492190:web:67b7606ff790064ef3250f"
+  appId: "1:884858492190:web:67b7606ff790064ef3250f",
+  measurementId: "G-J73RD8EH7W"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-export { db, ref, get, child, onValue };
+export { db, ref, set, get, child, onValue };
