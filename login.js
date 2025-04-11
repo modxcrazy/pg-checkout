274f469ca9fb393d5dee7a47544f652f5e1f4070
modxcrazy <in99soft.dev.pvt.ltd@gmail.com>
2025-04-11 09:37:31 +0530

    Update and rename firebase_config.js to login.js

---
+++ login.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCn553qWsVz2VF1dZ4Ji5OkQDGFvMORbJE",
+  authDomain: "pg-data-ed1c2.firebaseapp.com",
+  databaseURL: "https://pg-data-ed1c2-default-rtdb.firebaseio.com",
+  projectId: "pg-data-ed1c2"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+document.getElementById("loginBtn").addEventListener("click", () => {
+  const email = document.getElementById("loginEmail").value.trim();
+  const password = document.getElementById("loginPassword").value.trim();
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "admin.html";
+    })
+    .catch(err => {
+      document.getElementById("loginError").textContent = "Invalid credentials!";
+    });
+});
